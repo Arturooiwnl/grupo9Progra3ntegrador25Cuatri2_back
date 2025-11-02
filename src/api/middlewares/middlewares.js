b669679c3402dd882921c2fd36a3aa14ee24e3a4
JoNaX KaY <jonatan.quiroga.utn@gmail.com>
2025-11-02 19:18:07 -0300

    fix imagenes

--- src/api/middlewares/middlewares.js
+++ src/api/middlewares/middlewares.js
@@ -1,5 +1,10 @@
 import cors from "cors";
+import express from "express";
 
 export default function middlewares(app) {
     app.use(cors());
+    app.use(express.json()); //parsear json y poder recivir datos en formato json
+    app.use(express.urlencoded({ extended: true })); //parsear formularios y poder recivir datos en formulario
+    app.use('/uploads', express.static('./src/uploads')); //sirvo mis imagenes
+
 }
