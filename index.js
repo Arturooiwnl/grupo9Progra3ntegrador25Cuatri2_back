f4adfa4ba2471b07bb3bc93d030d21649bf6efcd
JoNaX KaY <jonatan.quiroga.utn@gmail.com>
2025-11-10 18:58:58 -0300

    reorganize backend into modular structure (routes, middlewares)

--- index.js
+++ index.js
@@ -1,148 +1,26 @@
 import express from "express"; //importamos el framework express
 const app = express();
-import environments from "./src/api/config/environments.js"; // importamos las variables de entonro
-import connection from "./src/api/database/db.js";//importamos la conexion a la base de datos
-import middlewares from "./src/api/middlewares/middlewares.js";
 
+import environments from "./src/api/config/environments.js"; // importamos las variables de entonro
 const PORT = environments.port;
+
+import cors from "cors";
+import { loggerUrl } from "./src/api/middlewares/middlewares.js"; 
+import { productRoutes } from "./src/api/routes/index.js";
+
 
 //Middlewares//
 
-middlewares(app);
-
-//ENDPOINT//
-
-app.get("/productos", async (req, res) => {
-    try {
-        const sql = `SELECT * FROM productos`;
-        /*
-        const resultado = await connection.query(sql);
-        console.log(resultado)
-        */
-
-        const [rows] = await connection.query(sql);
-        console.log(rows);
-
-        res.status(200).json({
-            payload: rows,
-            message: rows.length === 0? "no se encontraron productos" : "productos encontrados"
-        })
-
-    } catch(error) {
-        console.error(error);
-
-        res.status(500).json({
-            message:"Error interno al obtener productos"
-        })
-    }
-});
+    app.use(cors());
+    app.use(express.json()); //parsear json y poder recivir datos en formato json
+    app.use(loggerUrl);
+    app.use(express.urlencoded({ extended: true })); //parsear formularios y poder recivir datos en formulario
+    app.use('/uploads', express.static('./src/uploads')); //sirvo mis imagenes
 
 
-app.get("/productos/:id", async (req, res) => {
-    try {
-        let {id} = req.params; // esto me permite obtener el valor numerico despues de productos/productos/2
+    //RUTAS//
 
-        let sql = `SELECT * FROM productos where id = ?`;
-        const [rows] = await connection.query(sql, [id]); // el id reemplaza el ?
-
-        res.status(200).json({
-            payload: rows
-        })
-
-    } catch (error) {
-        console.error("Error obteniendo producto ID". error, message);
-
-        res.status(500).json({
-            message:"Error"
-        })
-    }
-})
-
-
-app.post("/productos", async (req, res) => {
-    try {
-        const { nombre, imagen, categoria, precio} = req.body;
-        // Aca imprimimos lo que enviamos desde el form que previamente se parseo gracias al middleware -> express.json()
-        console.log(req.body);
-
-        // Los placeholders ?, evitan inyecciones SQL para evitar ataques de este tipo
-        let sql = "INSERT INTO productos (nombre, imagen, categoria, precio) VALUES (?,?,?,?)";
-
-        //le envio estos valores a la DB
-        let [rows] = await connection.query(sql, [nombre, imagen, categoria, precio]);
-
-        //devuelvo una respuesta 201 "created"
-        res.status(201).json({
-            message: "producto encontrado con exito"
-        });
-
-    } catch(error) {
-        console.error("error interno del servidor",error);
-
-        res.status(500).json({
-            message:"error interno del servidor",
-            error: error.message
-        })
-    }
-});
-
-
-app.put("/productos", async (req, res) => {
-    try {
-        let {id, nombre, categoria, precio, imagen} = req.body;
-        
-        let sql = `
-        UPDATE productos
-        set nombre = ?, categoria = ?, precio = ?, imagen = ?
-        WHERE id = ?
-        `;
-        
-        let [result] = await connection.query(sql, [nombre, categoria, precio, imagen, id]);
-        console.log(result);
-
-        res.status(200).json({
-            message: "Producto actualizo correctamente"
-        });
-
-    } catch (error) {
-        console.error("error al actualizar el producto", error);
-
-        res.status(500).json({
-            message: "error al actualizar el producto",
-            error: error.message
-        })
-    }
-});
-
-
-app.delete("/productos/:id", async (req,res) => {
-    try {
-        let { id } = req.params;
-
-        //aca puedo hacer un borrado normal
-        let sql = "DELETE FROM productos WHERE id = ?";
-
-        //o puedo hacer una baja logica
-        //let sql2 = "UPDATE products set active = 0 WHERE id = ?";
-
-        let [result] = await connection.query(sql, [id]);
-        console.log(result);
-
-        return res.status(200).json({
-            message: `Producto con id ${id} eliminado correctamente`
-        });
-
-
-    } catch(error){
-        console.log(`Error al eliminar producto con id ${id}: `, error);
-
-        res.status(500).json({
-            message: `Error al eliminar un producto con id ${id}`,
-            error: error.message
-        })
-    }
-});
-
+app.use("/productos", productRoutes )
 
 
 
